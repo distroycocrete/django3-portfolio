bd2c7e74ff81c43a7d0911d116ede26c55643ef4
bikwal <bikwal@mail.ru>
2022-04-04 09:45:11 +0000

    Added a gitignore

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5!e^($^9ux%jk7sctss@-0xt8$n&t-6-h_d$f3_87ey*s-apx&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bikwal.pythonanywhere.com']
 
 
 # Application definition
@@ -118,8 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT= (BASE_DIR / 'media')
-MEDIA_URL= '/media/'
+STATIC_ROOT = (BASE_DIR / 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (BASE_DIR / 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
